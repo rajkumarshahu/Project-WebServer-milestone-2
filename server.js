c9b77c76ce9822acd959c4855af26f71e668f0db
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-10-16 14:46:17 -0400

    Create basic routes and checked for response

--- server.js
+++ server.js
@@ -6,9 +6,30 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/patients', (req, res) =>{
+    res.status(200).json({ success:true, message: 'Show all patients'});
+});
+
+app.get('/patients/:id', (req, res) =>{
+    res.status(200).json({ success:true, message: `Get a patient ${req.params.id}`});
+});
+
+app.post('/patients', (req, res) =>{
+    res.status(200).json({ success:true, message: 'Add a new patient'});
+});
+
+app.put('/patients/:id', (req, res) =>{
+    res.status(200).json({ success:true, message: `Get a patient ${req.params.id}`});
+});
+
+app.delete('/patients/:id', (req, res) =>{
+    res.status(200).json({ success:true, message: `Delete a patient ${req.params.id}`});
+});
+
+
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
+app.listen(
 	PORT,
 	console.log(
 		`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
