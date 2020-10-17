8518c68c3c73a8bf37eed056bae292765ab34fe0
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-10-17 10:53:59 -0400

    Create patient model

---
+++ models/Patient.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters'],
+    },
+    age:{
+        type: Number,
+        required: [true, 'Please add age']
+    },
+
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters'],
+    },
+    email: {
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email',
+        ],
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address'],
+    },
+    diagnosis: {
+        type: String,
+        required: [true, 'Please add diagnosis'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters'],
+    },
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Patient', PatientSchema);
